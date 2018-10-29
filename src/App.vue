96693cab8c6c768b46fc4c9571dfa8ebcbafc0a7
Mark Barr <mark@hyperingenuity.com>
2018-10-29 18:21:24 -0400

    Updated template.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app id="app" dark>
+  <v-navigation-drawer v-model="drawer" clipped fixed app>
+    <v-list dense>
+      <v-list-tile @click="true">
+        <v-list-tile-action>
+          <v-icon>mdi-dashboard</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Dashboard</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile @click="true">
+        <v-list-tile-action>
+          <v-icon>mdi-settings</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Settings</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <v-toolbar app fixed clipped-left dense color="blue-grey darken-1">
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title>Athena</v-toolbar-title>
+  </v-toolbar>
+  <v-content>
+    <router-view></router-view>
+  </v-content>
+  <v-footer app fixed>
+    <span class="pl-2">&copy; 2018</span>
+  </v-footer>
+</v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.athena-logo {
+    font-size: 12px;
 }
 </style>

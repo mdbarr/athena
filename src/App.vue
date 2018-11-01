cae422015d81816f4e4b7d57da944bbbedcd3459
Mark Barr <mark@hyperingenuity.com>
2018-11-01 17:40:09 -0400

    Placeholder components, color change.

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,31 @@
       <img src="./assets/athena.svg" width="32" color="white">
     </v-btn>
     <v-toolbar-title class="satyr">ATHENA</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-menu offset-y>
+      <v-btn slot="activator" flat>
+        Mark &nbsp; <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+      <v-list>
+        <v-list-tile @click="navigate('profile')">
+          <v-list-tile-title>Profile</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile @click="navigate('settings')">
+          <v-list-tile-title>Settings</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile @click="logout()">
+          <v-list-tile-title>Logout</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
   </v-toolbar>
   <v-content>
     <router-view></router-view>
   </v-content>
-  <v-footer app fixed>
-    <span class="pl-2">&copy; 2018</span>
+  <v-footer app fixed class="pa-3" height="28">
+    <span>&copy; 2018</span>
+    <v-spacer></v-spacer>
+    <div><span class="green--text">&#9679;</span> Connected</div>
   </v-footer>
   <img src="./assets/owl.svg" class="owl">
 </v-app>
@@ -61,15 +80,16 @@ export default {
 }
 
 .athena-toolbar {
+    background-image: linear-gradient(to right, #243949 0%, #517fa4 100%);
     background-image: linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%);
+    background-image: linear-gradient(to right, #3f647f 0%, #3f637f 100%);
 }
 
 .owl {
-    position: absolute;
+    position: fixed;
     width: 400px;
     right: 40px;
     bottom: 40px;
     z-index: 0;
 }
-
 </style>

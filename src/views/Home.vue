f7559e771b867a649825bf6e7a61babd6c92aeb2
Mark Barr <mark@hyperingenuity.com>
2018-11-04 22:11:49 -0500

    Dependency upgrade and UI prototype.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-   <div>Empty</div>
+<v-layout>
+  <v-flex xs12 sm2 offset-sm3 class="pa-3">
+    <v-card md-2>
+      <v-card-title class="blue">Server #1</v-card-title>
+      <v-card-text>Status</v-card-text>
+    </v-card>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>

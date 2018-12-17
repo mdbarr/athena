8e642ac02429ff7a7f07404cc24e964645ee7612
Mark Barr <mark@hyperingenuity.com>
2018-12-17 00:24:54 -0500

    Refactored Broken webhook trigger.

--- server/triggers/webhook.js
+++ server/triggers/webhook.js
@@ -1,17 +1,39 @@
 'use strict';
 
+const defaultMethod = 'post';
+const methods = [
+  'del',
+  'get',
+  'head',
+  'options',
+  'patch',
+  'post',
+  'put'
+];
+
 function Webhook(athena, node, options, id) {
   const url = `/api/triggers/${ node.id }`;
 
-  const trigger(req, res, next) {
+  options = options.split(/,\s*/);
+  if (!options.length) {
+    options.push(defaultMethod);
+  }
+
+  const trigger = function(req, res, next) {
     node.trigger(id);
 
     res.send(200);
     next();
-  }
+  };
 
-  athena.api.post(url, trigger);
-  athena.api.put(url, trigger);
+  for (let method in options) {
+    method = method.trim();
+    if (methods.includes(method)) {
+      athena.api[method](url, trigger);
+    } else {
+      console.log('Unknown HTTP method for webhook trigger', method);
+    }
+  }
 }
 
 module.exports = Webhook;

3c78c5875dedf41802796c3f02f09d0cb3e7f8bb
Mark Barr <mark@hyperingenuity.com>
2019-01-06 20:42:12 -0500

    Test node added with cyclic statuses.

---
+++ server/modules/test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  name: 'test',
+  dependencies: 'node',
+  load: function(athena) {
+    class Test extends athena.nodes.Node {
+      constructor(options = {}) {
+        super(options);
+
+        const node = this;
+
+        node.config.type = 'test';
+        node.config.icon = options.icon || 'fire';
+        node.config.folderIcon = options.folderIcon || 'fire';
+        node.config.status = 'own';
+
+        let healths = [];
+
+        node.on('trigger', function() {
+          if (!healths.length) {
+            healths = Object.keys(athena.constants.health);
+          }
+
+          const health = healths.pop();
+          const description = `Test health: ${ health }`;
+
+          node.update({
+            health,
+            description
+          });
+        });
+      }
+    }
+
+    athena.nodes.register('test', Test);
+  }
+};
